fa8964d5bf0e4826d822b1520ee569a551d8259d
niko <crap@crap.com>
2019-07-15 17:15:09 -0400

    clean

--- build/stb_image.d
+++
@@ -1,5 +0,0 @@
-stb_image.o: \
- D:/Profiles/njp2424/Desktop/SwitchGraphics/OpenGL_TexturedCube/source/stb_image.cpp \
- D:/Profiles/njp2424/Desktop/SwitchGraphics/OpenGL_TexturedCube/source/stb_image.h
-
-D:/Profiles/njp2424/Desktop/SwitchGraphics/OpenGL_TexturedCube/source/stb_image.h:
